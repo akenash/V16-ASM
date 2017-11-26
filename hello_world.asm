1934240a45edfe16cf33f2e3d1570b4031974494
zukonake <zukonake@protonmail.com>
2017-11-26 01:09:14 +0100

    Optimized hello_world a bit

--- hello_world.asm
+++ hello_world.asm
@@ -1,4 +1,3 @@
-CPY  A:0x0005  R:0x0000 # 0x0005 is the iterator for binary data, R[0] will hold it
 JMP  A:0x0009           # jump over the binary data
 
 0x6548
@@ -9,6 +8,7 @@ JMP  A:0x0009           # jump over the binary data
 0x2164
 0x000A
 
+CPY  S:0x0007  R:0x0000 # copy the iterator for binary data to R[0]
 CPY $R:0x0000  H:0x0100 # copy the binary data from at address R[0] to HW
 ADD  M:0x0001 $S:0x0001 # add 1 to the HW iterator
 ADD  M:0x0001  R:0x0000 # add 1 to the binary data iterator
@@ -17,4 +17,4 @@ JMP  S:0x000C           # jump to beginning of copying
 API  M:0x0001  M:0x0001 # send the write signal to console
 API  M:0x0002  M:0x0001 # send the clear (buffer) signal to console
 CPY  M:0x0100 $S:0x0012 # reset the HW iterator
-JMP  S:0x0023           # jump to the beginning of the program
+JMP  S:0x001A           # jump to the beginning of the program

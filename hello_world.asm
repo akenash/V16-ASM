b2ee23c1a5b50bfdebf6f36284660e72cb687a69
zukonake <zukonake@protonmail.com>
2017-11-26 13:43:02 +0100

    improvements to .asm files

--- hello_world.asm
+++ hello_world.asm
@@ -1,13 +1,13 @@
 BINREG:0x0000
 
 # program start
-CPY  A:0x0016  R%BINREG # copy the iterator for binary data
+CPY  A:0x0013  R%BINREG # copy the iterator for binary data
 # loop start
-CPY $R%BINREG  H:0x0100 # copy the binary data from at address R[0] to HW
-ADD  M:0x0001 $S:0x0001 # add 1 to the binary data iterator
-ADD  M:0x0001  R%BINREG # add 1 to the binary data iterator
-IGQ $S:0x0007  M:0x0107 # if we copied 7 words, skip the jump
-JMP  S:0x000C           # jump to beginning of copying
+CPY $I%BINREG  H:0x0100 # copy the binary data at address of binreg to hw, increment binreg
+ADD  M:0x0001 $S:0x0001 # add 1 to the HW data iterator
+IGQ  R%BINREG  A:0x0011 # if the binreg is at the end of binary data, exit the loop
+JMP  S:0x0009           # jump to beginning of copying
+# loop end
 API  M:0x0001  M:0x0001 # send the write signal to console
 JMP  S:0x0003           # print again
 
--- memcpy.asm
+++ memcpy.asm
@@ -2,11 +2,10 @@ SRCREG:0x0000
 DSTREG:0x0001
 SIZREG:0x0002
 ITEREG:0x0003
-MOV  M:0x0000  R%ITEREG
-CPY $M%SRCREG $M%DSTREG
-ADD  M:0x0001  R%SRCREG
-ADD  M:0x0001  R%DSTREG
-ADD  M:0x0001  R%ITEREG
-IGQ  R%ITEREG  R%SIZREG
-JMP  S:0x000F
+
+MOV  M:0x0000  R%ITEREG # iterator = 0
+# loop start
+CPY $I%SRCREG $I%DSTREG # copy from source to destination and increment both iterators
+ILT  I%ITEREG  R%SIZREG # if iterator < size skip return
 RET
+JMP  S:0x0007

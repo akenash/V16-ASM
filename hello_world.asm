5feb066b1be9371639b772673d18e701a740f773
zukonake <zukonake@protonmail.com>
2017-11-26 12:32:59 +0100

    Fixed hello_world and added example memcpy.asm

--- hello_world.asm
+++ hello_world.asm
@@ -1,4 +1,15 @@
-JMP  A:0x0009           # jump over the binary data
+BINREG:0x0000
+
+# program start
+CPY  A:0x0016  R%BINREG # copy the iterator for binary data
+# loop start
+CPY $R%BINREG  H:0x0100 # copy the binary data from at address R[0] to HW
+ADD  M:0x0001 $S:0x0001 # add 1 to the binary data iterator
+ADD  M:0x0001  R%BINREG # add 1 to the binary data iterator
+IGQ $S:0x0007  M:0x0107 # if we copied 7 words, skip the jump
+JMP  S:0x000C           # jump to beginning of copying
+API  M:0x0001  M:0x0001 # send the write signal to console
+JMP  S:0x0003           # print again
 
 0x6548
 0x6C6C
@@ -8,11 +19,3 @@ JMP  A:0x0009           # jump over the binary data
 0x2164
 0x000A
 
-CPY  S:0x0007  R:0x0000 # copy the iterator for binary data to R[0]
-CPY $R:0x0000  H:0x0100 # copy the binary data from at address R[0] to HW
-ADD  M:0x0001 $S:0x0001 # add 1 to the HW iterator
-ADD  M:0x0001  R:0x0000 # add 1 to the binary data iterator
-IGQ $S:0x0007  M:0x0107 # if we copied 7 words, skip the jump
-JMP  S:0x000C           # jump to beginning of copying
-API  M:0x0001  M:0x0001 # send the write signal to console
-JMP  S:0x0003           # jump to the beginning of the program
